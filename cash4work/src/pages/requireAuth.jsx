a5da1221af33f243f5441da70bf6621616f896f6
Fahad <99376771+anik1204@users.noreply.github.com>
2023-03-22 03:03:08 -0400

    Verify token with backend

--- cash4work/src/pages/requireAuth.jsx
+++ cash4work/src/pages/requireAuth.jsx
@@ -1,22 +1,44 @@
-import React from "react"
+import { React, useRef } from "react"
 import { Navigate, Outlet, useLocation } from "react-router-dom"
 import Navbar from "../components/navbar"
 import { useAuthContext } from "../context"
+const API_URL = "https://cash4-work-backend.vercel.app";
+
 
 export default function RequireAuth() {
   const location = useLocation()
   const { user } = useAuthContext()
-
-  const isAuthenticated = () => {
-	console.log(localStorage.getItem("user"));
-    return user && user.token;
+  let Authenticated = useRef(false);
+  const isAuthenticated = async () => {
+    console.log(user);
+    if (user) {
+      try {
+        const response = await fetch(API_URL + "/user/verify", {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify( {token: user.token, email: user.email} ),
+        });
+        if (response.ok) {
+          Authenticated = true;
+          return;
+        } else {
+          Authenticated = false;
+          return;
+        }
+      } catch (error) {
+        console.log(error);
+        Authenticated = false;
+        return;
+      }
+    }
+    else Authenticated = false;
   }
-
+  isAuthenticated();
   if (location.pathname === "/login" || location.pathname === "/signup") {
-    return isAuthenticated() ? <Navigate to="/jobs" /> : <Outlet />
+    return Authenticated ? <Navigate to="/jobs" /> : <Outlet />
   }
 
-  return isAuthenticated() ? (
+  return Authenticated ? (
     <div className="bg-gray-100">
       <Navbar />
       <div className="max-w-6xl mx-auto">
